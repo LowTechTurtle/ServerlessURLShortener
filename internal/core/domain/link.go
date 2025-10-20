6eebee7f496f982a72e6494c8d42cab445f34919
LowTechTurtle <dongquocthang1235@gmail.com>
2025-10-20 20:36:15 +0700

    Initial commit, add domain in hexagonal architecture

---
+++ internal/core/domain/link.go
@@ -0,0 +1,9 @@
+package domain
+
+import "time"
+
+type Link struct {
+	Id          string    `dynamodbav:"id" json:"id"`
+	OriginalURL string    `dynamodbav:"original_url" json:"original_url"`
+	CreatedAt   time.Time `dynamodbav:"created_at" json:"created_at"`
+}

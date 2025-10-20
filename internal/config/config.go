962fbd3fd9383a138504223b17ce67b831463b0e
LowTechTurtle <dongquocthang1235@gmail.com>
2025-10-20 21:04:08 +0700

    Handle env var for redis

---
+++ internal/config/config.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type AppConfig struct {
+	dynamoTableName string // DynamoDB table name
+	redisAddress    string // Redis address
+	redisPassword   string // Redis password
+	redisDB         int    // Redis DB
+}
+
+func NewConfig() *AppConfig {
+	return &AppConfig{
+		dynamoTableName: "UrlShortenerTable", // default value
+		redisAddress:    "localhost:6379",    // default value
+		redisPassword:   "",                  // default value
+		redisDB:         0,                   // default value
+	}
+}
+
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Print("Error loading .env file: ", err)
+	}
+}
+
+func (c *AppConfig) GetLinkTableName() string {
+	tableName, ok := os.LookupEnv("LinkTableName")
+	if !ok {
+		fmt.Println("Need LinkTableName environment variable")
+		return os.Getenv("LinkTableName")
+	}
+	return tableName
+}
+
+func (c *AppConfig) GetRedisParams() (string, string, int) {
+	address, ok := os.LookupEnv("RedisAddress")
+	if !ok {
+		fmt.Println("Need RedisAddress environment variable")
+		return c.redisAddress, c.redisPassword, c.redisDB
+	}
+
+	password, ok := os.LookupEnv("RedisPassword")
+	if !ok {
+		fmt.Println("Need RedisPassword environment variable")
+		return address, c.redisPassword, c.redisDB
+	}
+
+	dbStr, ok := os.LookupEnv("RedisDB")
+	if !ok {
+		fmt.Println("Need RedisDB environment variable")
+		return address, password, c.redisDB
+	}
+
+	db, err := strconv.Atoi(dbStr)
+	if err != nil {
+		fmt.Printf("RedisDB environment variable is not a valid integer: %v\n", err)
+		return address, password, c.redisDB
+	}
+
+	return address, password, db
+}

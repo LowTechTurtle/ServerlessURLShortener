f66cd9553ef5cd57e0ead6eefde058fd78aa8417
LowTechTurtle <dongquocthang1235@gmail.com>
2025-10-20 21:26:16 +0700

    add go mod, go sum, and some lambda function for AWS

---
+++ internal/adapters/functions/redirect/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+
+	"github.com/LowTechTurtle/ServerlessURLShortener/internal/adapters/cache"
+	"github.com/LowTechTurtle/ServerlessURLShortener/internal/adapters/handlers"
+	"github.com/LowTechTurtle/ServerlessURLShortener/internal/adapters/repository"
+	"github.com/LowTechTurtle/ServerlessURLShortener/internal/config"
+	"github.com/LowTechTurtle/ServerlessURLShortener/internal/core/services"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	appConfig := config.NewConfig()
+	redisAddress, redisPassword, redisDB := appConfig.GetRedisParams()
+	cache := cache.NewRedisCache(redisAddress, redisPassword, redisDB)
+	linkTableName := appConfig.GetLinkTableName()
+
+	linkRepo := repository.NewLinkRepository(context.TODO(), linkTableName)
+	linkService := services.NewLinkService(linkRepo, cache)
+
+	handler := handlers.NewRedirectFunctionHandler(linkService)
+	lambda.Start(handler.Redirect)
+}
